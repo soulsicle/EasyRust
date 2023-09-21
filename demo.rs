3a12a0e9eb8ec8bdb2375c375f50d3f46009c4fd
puttyX <77942520+soulsicle@users.noreply.github.com>
2023-09-21 16:03:35 -0700

    Update

--- demo.rs
+++ demo.rs
@@ -1,6 +1,11 @@
 mod EasyRust;
 [#tokio main]
 async fn main() {
+    let number_example:i128 = 123456789012345678; 
     let get_request_example = EasyRust::webget("https://www.google.com/");
     println!(body);
+    println!("Break on int running...");
+    EasyRust::breakonint(number_example);
+    println!("writing to file test");
+    let file_example = EasyRust::write("test.txt", "Hello World!");
 }
